2b8e0cc152bf073b60b6fda0d17b20bebe56ee1e
HaradaTomohiro <taizo.bontakun.19850103@gmail.com>
2017-10-05 20:24:58 +0900

    change PEANUTS

--- src/scrum/Simulator.java
+++ src/scrum/Simulator.java
@@ -2,11 +2,11 @@ package scrum;
 
 public class Simulator {
 	
-	private static final int STORY_POINT = 100;
+	private static final int PEANUTS = 100;
 	private static final double SPRINT_PERIOD = 0.5;
 	
 	public double calc(int velocity) {
 		
-		return (STORY_POINT / velocity) * SPRINT_PERIOD;
+		return (PEANUTS / velocity) * SPRINT_PERIOD;
 	}
 }
--- src/waterfall/Simulator.java
+++ src/waterfall/Simulator.java
@@ -2,11 +2,11 @@ package waterfall;
 
 public class Simulator {
 	
-	private static final int STORY_POINT = 100;
+	private static final int PEANUTS = 100;
 
 	public int calc(int manMonth) {
 		
-		return STORY_POINT / manMonth;
+		return PEANUTS / manMonth;
 	}
 	
 	
