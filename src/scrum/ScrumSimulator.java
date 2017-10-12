7cdad990d27794ccccfb51bd5529aec2eebc6cee
HaradaTomohiro <taizo.bontakun.19850103@gmail.com>
2017-10-12 21:01:44 +0900

    add empty class

---
+++ src/scrum/ScrumSimulator.java
@@ -0,0 +1,10 @@
+package scrum;
+
+public class ScrumSimulator {
+		
+	private static final int PEANUTS = 100;
+
+	public double calc(){
+		return 0.0;
+	}
+}
---
+++ src/scrum/ScrumSimulatorTest.java
@@ -0,0 +1,16 @@
+package scrum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import scrum.ScrumSimulator;
+
+public class ScrumSimulatorTest {
+
+	//TODO ピーナッツは固定
+	//TODO ピーナッツに対し何スプリント必要かを算出
+	//TODO ベロシティを指定したら何スプリント必要かを算出
+	//TODO 何を渡したら(ベロシティ)何が返ってくるのか(ピーナッツすべてを完了させるのに何スプリント必要か)
+	//TODO 何を渡したら(ベロシティと、1スプリントの期間)何が返ってくるのか(期間(month)) → Happy end
+}
