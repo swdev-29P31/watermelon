ed58680a20d4a79b4551344d2b15c8c38b9bf7b3
tomoko <tomoko.sohda@jrits.ricoh.co.jp>
2017-10-19 19:39:59 +0900

    Refactoring Scrum Test

--- src/scrum/ScrumSimulatorTest.java
+++ src/scrum/ScrumSimulatorTest.java
@@ -5,17 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ScrumSimulatorTest {
+	private int velocity = 8;
+	private double sprintTerm = 0.5;
 
 	@Test
 	public void 計算ロジックテスト_最終結果() {
 		double result = 0;
 		ScrumSimulator ss = new ScrumSimulator();
-
-		int velocity = 8;
-		double sprintTerm = 0.5;
 		double expectedTerm = 6.5;
 
-		result = ss.calcTerm(velocity, sprintTerm);
+		result = ss.calcTerm(this.velocity, this.sprintTerm);
 		assertEquals(expectedTerm, result, 0);
 	}
 
@@ -23,11 +22,9 @@ public class ScrumSimulatorTest {
 	public void スプリントを計算するテスト_最終結果() {
 		int result = 0;
 		ScrumSimulator ss = new ScrumSimulator();
-
-		int velocity = 8;
 		int expextedSprintTime = 13;
 
-		result = ss.calcSprint(velocity);
+		result = ss.calcSprint(this.velocity);
 		assertEquals(expextedSprintTime, result);
 	}
 
