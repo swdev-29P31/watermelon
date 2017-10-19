aba40b06ded6ef1d45dc55615d6e552d1bd2934f
tomoko <tomoko.sohda@jrits.ricoh.co.jp>
2017-10-19 19:35:28 +0900

    Added Calc Methods

--- src/scrum/ScrumSimulator.java
+++ src/scrum/ScrumSimulator.java
@@ -1,10 +1,14 @@
 package scrum;
 
 public class ScrumSimulator {
-		
+
 	private static final int PEANUTS = 100;
 
-	public double calc(){
-		return 0.0;
+	public double calcTerm(int velocity, double sprintTerm) {
+		return calcSprint(velocity) * sprintTerm;
+	}
+
+	public int calcSprint(int velocity) {
+		return (int) Math.ceil((double) PEANUTS / (double) velocity);
 	}
 }
--- src/scrum/ScrumSimulatorTest.java
+++ src/scrum/ScrumSimulatorTest.java
@@ -6,15 +6,6 @@ import org.junit.Test;
 
 public class ScrumSimulatorTest {
 
-	// TODO ピーナッツは固定
-	// TODO ピーナッツに対し何スプリント必要かを算出
-	// TODO ベロシティを指定したら何スプリント必要かを算出
-	// TODO (1) 何を渡したら(ベロシティ)何が返ってくるのか(ピーナッツすべてを完了させるのに何スプリント必要か)
-	// TODO (2) 何を渡したら(ベロシティと、1スプリントの期間)何が返ってくるのか(期間(month)) → Happy end
-
-	// TODO 10/12追加 TODO(1)、(2)メソッドを分ける(10/19にこれやる)
-	// 10/12時点では、（1）、(2)をまとめてcalcで計算するように設計されている。
-
 	@Test
 	public void 計算ロジックテスト_最終結果() {
 		double result = 0;
@@ -22,13 +13,22 @@ public class ScrumSimulatorTest {
 
 		int velocity = 8;
 		double sprintTerm = 0.5;
+		double expectedTerm = 6.5;
 
-		// TODO 作成途中(メソッドを分けた後に続きをすること！)
-		// result = ss.calc(velocity, sprintTerm);
-		result = ss.calc();
-
-		// assertEquals(6.5, result, 0);
-		assertEquals(0.0, result, 0);
-
+		result = ss.calcTerm(velocity, sprintTerm);
+		assertEquals(expectedTerm, result, 0);
 	}
+
+	@Test
+	public void スプリントを計算するテスト_最終結果() {
+		int result = 0;
+		ScrumSimulator ss = new ScrumSimulator();
+
+		int velocity = 8;
+		int expextedSprintTime = 13;
+
+		result = ss.calcSprint(velocity);
+		assertEquals(expextedSprintTime, result);
+	}
+
 }
