bd503978db57e3994411a94666eda4017b04cc4d
HaradaTomohiro <taizo.bontakun.19850103@gmail.com>
2017-11-09 18:36:05 +0900

    delete old package.

--- src/waterfall/TestPhase.java
+++
@@ -1,7 +0,0 @@
-package waterfall;
-
-public class TestPhase extends WaterfallSimulator{
-	public double calc(int testPeanuts, int people) {
-		return (PEANUTS * TEST_PEANUTS_RATE) / (testPeanuts * people);
-	}
-}
