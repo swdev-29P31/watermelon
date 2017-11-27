bb30a51729b151518986cbc5f13c9f442b6a57ce
tomoko <tomoko.sohda@jrits.ricoh.co.jp>
2017-11-27 20:54:31 +0900

    スクラムとWaterfallを比較できるようにした。

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,29 +1,60 @@
 package simulator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Simulator {
 
 	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		//WFの設計期間を出す
+		double inputDesignPeriod = 0.0;
+		double inputImplimentationPeriod = 0.0;
+		double inputAcceptancePeriod = 0.0;
+		try {
+			System.out.print("Waterfallでの設計期間を入れてください: ");
+			String input = br.readLine();
+			inputDesignPeriod = Double.parseDouble(input);
+
+			System.out.print("Waterfallでの製造期間を入れてください: ");
+			input = br.readLine();
+			inputImplimentationPeriod = Double.parseDouble(input);
+
+			System.out.print("Waterfallでのテスト期間を入れてください: ");
+			input = br.readLine();
+			inputAcceptancePeriod = Double.parseDouble(input);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("失敗しました。");
+			e.printStackTrace();
+		}
+
+		// WF
 		WaterfallDesign wd = new WaterfallDesign();
-		System.out.println("Design Period = " + wd.calcDesignPeriod());
+		wd.setDesignPeriod(inputDesignPeriod);
+		System.out.println("Design People: " + wd.calcDesignPeople());
 
-		//WFの実装期間を出す
 		WaterfallImplimentation wi = new WaterfallImplimentation();
-		System.out.println("Implimentation Period = " + wi.calcImplimentationPeriod());
+		wi.setImplimentationPeriod(inputImplimentationPeriod);
+		System.out.println("Implimentation People: " + wi.calcImplimentationPeople());
 
-		//WFのテスト期間を出す
 		WaterfallAcceptance wa = new WaterfallAcceptance();
-		System.out.println("Acceptance Period = " + wa.calcAcceptancePeriod());
+		wa.setAcceptancePeriod(inputAcceptancePeriod);
+		System.out.println("Acceptance People: " + wa.calcAcceptancePeople() + "\n");
 
-		//WFの合計期間を出す
-		System.out.println("Waterfall Period = "
-				+ (wd.calcDesignPeriod() + wi.calcImplimentationPeriod() + wa.calcAcceptancePeriod()) +"\n");
+		// WFの合計期間を出す
+		// System.out.println("Waterfall Period: "
+		// + (wd.calcDesignPeriod() + wi.calcImplimentationPeriod() +
+		// wa.calcAcceptancePeriod()) + "\n");
 
-		//Scrumの期間を出す
+		// Scrum
+		double totalPeriod = inputDesignPeriod + inputImplimentationPeriod + inputAcceptancePeriod;
+		System.out.println("Scrumチームに与えられた期間: " + totalPeriod + "ヶ月");
 		ScrumTeam st = new ScrumTeam();
-		System.out.println("Scurm Period = " + st.calcTotalPeriod());
-
+		st.setTotalPeriod(totalPeriod);
+		System.out.println("今回必要なVelocity: " + st.calcVelocity());
 	}
 
 }
