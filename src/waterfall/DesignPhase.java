cdab2b23fca75afba5b6023f84f8333dfb5c5ac5
akitakun <tantan_k@zenno.biz>
2017-11-27 19:19:38 +0900

    不要なパッケージを削除

--- src/waterfall/DesignPhase.java
+++
@@ -1,8 +0,0 @@
-package waterfall;
-
-public class DesignPhase extends WaterfallSimulator{
-	
-	public double calc(int designPeanuts, int people){
-		return (PEANUTS * DESIGN_PEANUTS_RATE) / (designPeanuts * people);
-	}
-}
--- src/waterfall/MakePhase.java
+++
@@ -1,7 +0,0 @@
-package waterfall;
-
-public class MakePhase extends WaterfallSimulator{
-	public double calc(int makePeanuts, int people) {
-		return (PEANUTS * MAKE_PEANUTS_RATE) / (makePeanuts * people);
-	}
-}
